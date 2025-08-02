aaebc2ba916bb290cec498add3f146455ba3aa95
maxBRT <bourret74@gmail.com>
2025-08-01 21:18:58 -0400

    started working on the front end

--- main.go
+++ main.go
@@ -2,10 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/maxBRT/todo/internals/views"
 )
 
 func main() {
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	// Serve static files from the "./assets" directory
+	fs := http.FileServer(http.Dir("./assets"))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-	fmt.Println("Hello World!")
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		views.Index().Render(r.Context(), w)
+	})
+	mux.HandleFunc("POST /api/tasks", func(w http.ResponseWriter, r *http.Request) {
+		if err := r.ParseForm(); err != nil {
+			log.Printf("Error parsing the form: %s", err)
+		}
 
+		title := r.FormValue("title")
+		description := r.FormValue("description")
+		dueDate := r.FormValue("due-date")
+
+		fmt.Println(title)
+		fmt.Println(description)
+		fmt.Println(dueDate)
+
+		w.WriteHeader(http.StatusAccepted)
+		views.TaskForm().Render(r.Context(), w)
+	})
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
